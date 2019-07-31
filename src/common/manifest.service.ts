d29b463e5340c28256d92844a86d51da63aec7c3
Anton Kovalyov <thebyagent@gmail.com>
2019-07-31 13:45:34 -0700

    WIP on manifest validation.

--- src/common/manifest.service.ts
+++ src/common/manifest.service.ts
@@ -1,6 +1,21 @@
 import { ConfigurationStorage, ConfigurationType } from './storage.service';
 import { IManifest } from './types';
 
+type Validator = (manifest: IManifest) => null | IManifestValidationError;
+
+const ManifestMetadata = {
+  availableVersions: [1],
+};
+
+enum ValidationErrorCode {
+  InvalidVersion,
+  InvalidCascade,
+}
+
+interface IManifestValidationError {
+  code: number;
+  description: string;
+}
 
 class ManifestService {
   private configurationStorage: ConfigurationStorage;
@@ -18,5 +33,38 @@ class ManifestService {
   }
 }
 
-export { ManifestService };
+class ManifestValidationService {
+  private manifest: IManifest;
+  private validators: Validator[] = [this.checkVersion];
 
+  public constructor(manifest: IManifest) {
+    this.manifest = manifest;
+  }
+
+  public validate(): null | IManifestValidationError[] {
+    const manifest = this.manifest;
+    const errors: IManifestValidationError[] = [];
+
+    for (let validator of this.validators) {
+      const error = validator(manifest);
+      error ? errors.push(error) : null;
+    }
+
+    if (errors.length > 0) {
+      return errors;
+    }
+    return null;
+  }
+
+  private checkVersion(manifest: IManifest): null | IManifestValidationError {
+    if (!ManifestMetadata.availableVersions.includes(Number(manifest.version))) {
+      return {
+        code: ValidationErrorCode.InvalidVersion,
+        description: `Unknown version: ${manifest.version}`,
+      };
+    }
+    return null;
+  }
+}
+
+export { ManifestService, ManifestValidationService };
