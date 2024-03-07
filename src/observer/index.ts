468b49968c29cfef2da9c9088ef6fde3bb1b89f7
AminTi <titiamin@icloud.com>
2024-03-07 06:37:44 +0100

    Resolve vulnerabilities and upgrade packages

--- src/observer/index.ts
+++ src/observer/index.ts
@@ -11,38 +11,46 @@ import {
 import * as SDK from 'azure-devops-extension-sdk';
 import { CascadingFieldsService } from '../common/cascading.service';
 import { ManifestService } from '../common/manifest.service';
+import { initial } from 'lodash';
 
+
+
+const initialize = async () => { 
+  const workItemFormService = await SDK.getService<IWorkItemFormService>(
+    WorkItemTrackingServiceIds.WorkItemFormService
+  );
+  const projectInfoService = await SDK.getService<IProjectPageService>(
+    CommonServiceIds.ProjectPageService
+  );
+  const project = await projectInfoService.getProject();
+  const manifestService = new ManifestService(project.id);
+  let manifest = await manifestService.getManifest();
+  if (manifest == null) {
+    manifest = Object.assign({}, ManifestService.defaultManifest);
+  }
+  const cascadingService = new CascadingFieldsService(workItemFormService, manifest.cascades);
+
+  const provider: IWorkItemNotificationListener = {
+    onLoaded: async () => await cascadingService.cascadeAll(),
+    onSaved: async () => await cascadingService.cascadeAll(),
+    onRefreshed: async () => await cascadingService.cascadeAll(),
+    onReset: async () => await cascadingService.cascadeAll(),
+    onUnloaded: async () => await cascadingService.resetAllCascades(),
+    onFieldChanged: async (fieldChangedArgs: IWorkItemFieldChangedArgs) => {
+      await cascadingService.performCascading(Object.keys(fieldChangedArgs.changedFields)[0]);
+    },
+  };
+
+  SDK.register<IWorkItemNotificationListener>(SDK.getContributionId(), provider);
+  await SDK.notifyLoadSucceeded();
+}
+
+
+await initialize();
 SDK.init({
   applyTheme: true,
   loaded: false,
-}).then(
-  async (): Promise<void> => {
-    const workItemFormService = await SDK.getService<IWorkItemFormService>(
-      WorkItemTrackingServiceIds.WorkItemFormService
-    );
-    const projectInfoService = await SDK.getService<IProjectPageService>(
-      CommonServiceIds.ProjectPageService
-    );
-    const project = await projectInfoService.getProject();
-    const manifestService = new ManifestService(project.id);
-    let manifest = await manifestService.getManifest();
-    if (manifest == null) {
-      manifest = Object.assign({}, ManifestService.defaultManifest);
-    }
-    const cascadingService = new CascadingFieldsService(workItemFormService, manifest.cascades);
+});
 
-    const provider: IWorkItemNotificationListener = {
-      onLoaded: async () => await cascadingService.cascadeAll(),
-      onSaved: async () => await cascadingService.cascadeAll(),
-      onRefreshed: async () => await cascadingService.cascadeAll(),
-      onReset: async () => await cascadingService.cascadeAll(),
-      onUnloaded: async () => await cascadingService.resetAllCascades(),
-      onFieldChanged: async (fieldChangedArgs: IWorkItemFieldChangedArgs) => {
-        await cascadingService.performCascading(Object.keys(fieldChangedArgs.changedFields)[0]);
-      },
-    };
 
-    SDK.register<IWorkItemNotificationListener>(SDK.getContributionId(), provider);
-    await SDK.notifyLoadSucceeded();
-  }
-);
+
